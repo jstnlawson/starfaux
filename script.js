838b847df84862da41fd3c96bb6cd2e80944ea49
Justin Lawson <jstn.lawson@gmail.com>
2024-10-22 12:49:28 -0500

    using css instead of draw to position ship

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ document.addEventListener('DOMContentLoaded', function() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    let spaceshipX = canvas.width / 2 - 25;
-    let spaceshipY = canvas.height - 200;
+    // let spaceshipX = canvas.width / 2 - 25;
+    // let spaceshipY = canvas.height - 200;
 
-    function drawSpaceship() {
-        spaceship.style.left = spaceshipX + 'px';
-        spaceship.style.top = spaceshipY + 'px';
-    }
+    // function drawSpaceship() {
+    //     spaceship.style.left = spaceshipX + 'px';
+    //     spaceship.style.top = spaceshipY + 'px';
+    // }
 
     function update() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawSpaceship();
+        // drawSpaceship();
         requestAnimationFrame(update);
     }
 
