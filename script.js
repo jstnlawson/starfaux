085e50b693df38adc740804211532b092510ecb6
Justin Lawson <jstn.lawson@gmail.com>
2024-11-03 15:08:26 -0600

    added controls menu

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ import {
 } from "./components/user-jet/user-jet.js";
 import { enemyAttackSequence } from "./components/enemy-jet/enemy-jet.js";
 
-
 document.addEventListener("DOMContentLoaded", function () {
-
+    const startScreen = document.querySelector(".start-screen");    
   const startButton = document.querySelector(".start-button");
+  const openControlsMenu = document.querySelector(".controls-menu__title");
+  const closeMenu = document.querySelector(".close-menu");
+  const groundMovement = document.querySelector(".ground-movement");
   const audio = new Audio("./audio/starfaux.mp3");
   audio.volume = 0.3;
 
@@ -16,11 +18,22 @@ document.addEventListener("DOMContentLoaded", function () {
     audio.play();
   }
 
-  startButton.addEventListener("click", () => {
-    startButton.style.display = "none";
-    enemyAttackSequence();
-    playAudio();
-    
+  groundMovement.style.display = "none";
+
+  openControlsMenu.addEventListener("click", () => {
+    const controlsMenu = document.querySelector(".controls-menu");
+    controlsMenu.style.display = "flex";
   });
 
+  closeMenu.addEventListener("click", () => {
+    const controlsMenu = document.querySelector(".controls-menu");
+    controlsMenu.style.display = "none";
+  });
+
+  startButton.addEventListener("click", () => {
+    startScreen.style.display = "none";
+    groundMovement.style.display = "block";
+    enemyAttackSequence();
+    playAudio();
+  });
 });
